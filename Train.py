76b441f25b0ab8ea1635d47e467f3c632315a6d7
Nguyễn Hồng Sơn <itdsiu21117@student.hcmiu.edu.vn>
2024-05-10 22:20:38 +0700

    new
    new

--- Train.py
+++ Train.py
@@ -21,8 +21,8 @@ train_data = DataLoader(train_set, batch_size = 64, shuffle=True)
 test_set = ds.AnimalDataset(root=data_root,train=False, transform = transform)
 test_data = DataLoader(test_set, batch_size=64, shuffle=False)
 
-net = arch.Resnet34(3, 10)
-optimizer = optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+net = arch.Resnet18(3, 10)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
 criterion = nn.CrossEntropyLoss()
 
 from datetime import datetime
